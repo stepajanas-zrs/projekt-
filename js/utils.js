323a15f4f28e444ec3b4c0f91bce4da1e9d44a86
Štěpán Janás <stepajanas@gmail.com>
2026-09-21 20:19:45 +0200

    Update utils.js

--- js/utils.js
+++ js/utils.js
@@ -1,19 +1,4 @@
-// Globální funkce
-function openModal(modalId) {
-    document.getElementById(modalId).classList.add('show');
-}
-
-function closeModal(modalId) {
-    document.getElementById(modalId).classList.remove('show');
-}
-
-window.onclick = function(event) {
-    if (event.target.classList.contains('modal')) {
-        event.target.classList.remove('show');
-    }
-}
-
-// LocalStorage
+// Globální funkce (už se nepoužívají, ale ponech pro kompatibilitu)
 function saveToStorage(key, value) {
     localStorage.setItem(key, JSON.stringify(value));
 }
